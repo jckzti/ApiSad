013aa9608ddd4f2f7cc84489e822659b6d68c892
jckzti <jckzti@gmail.com>
2021-02-03 18:42:50 -0300

    add commnet

--- ApiSad/Controllers/TempoController.cs
+++ ApiSad/Controllers/TempoController.cs
@@ -54,8 +54,8 @@ namespace ApiSad.Controllers
             Uri usuarioUri;
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44358");
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("https://localhost:5001");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")); //está dando erro caso nao ache a url, tratar
             System.Net.Http.HttpResponseMessage response = client.GetAsync("weatherforecast").Result;
             
             if (response.IsSuccessStatusCode)
